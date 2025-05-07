4378a8741c33aee956f7145588e4b8d6fb06749e
Gertrat <markoku2007@gmail.com>
2025-05-07 16:22:59 +0300

    Update run_shadow.py

--- run_shadow.py
+++ run_shadow.py
@@ -15,7 +15,7 @@ try:
         p.draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
-        print(p.len_good_edges)
+        print('Длина искомых ребер', p.len_good_edges)
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
